e152ec294e2a7c520405ba2f7dac78dffc210e2a
imatake <hemlvg+github.com@live.jp>
2017-02-18 17:38:10 +0900

    17年2月のリニューアル対応

--- annict.com/annict.com_programs_style.user.js
+++ annict.com/annict.com_programs_style.user.js
@@ -1,23 +1,20 @@
 // ==UserScript==
-// @name        Annict 放送予定表示 V2
+// @name        Annict 放送予定表示 V3
 // @description Annict の放送予定ページで放送前の表示色を変更します。また、放送予定の表示範囲を抑制します。
 // @namespace   imatake.github.io
 // @include     https://annict.com/programs
 // @author      imatake
-// @version     1.00.20160104
+// @version     3.00.20170218
 // @license     MIT License
 // @grant       none
 // ==/UserScript==
 (function () {
   // 放送前のスタイルを変更する（スタイルシートを埋め込み）
   var css = '/* 放送前のスタイルを変更 */' + `
-body.programs-index .a-content .a-programs .a-program-container.unbroadcasted .a-program .a-header .a-started-at {
-  color: #228B22;
-}
-body.programs-index .a-content .a-programs .a-program-container.unbroadcasted .a-progress .a-line {
+body.p-programs .c-program-list .is-unbroadcasted .c-program-progress__line {
   border-color: #228B22;
 }
-body.programs-index .a-content .a-programs .a-program-container.unbroadcasted .a-progress .a-line .a-point {
+body.p-programs .c-program-list .is-unbroadcasted .c-program-progress__point {
   background-color: #228B22;
 }
 `;
@@ -25,82 +22,67 @@ body.programs-index .a-content .a-programs .a-program-container.unbroadcasted .a
   if (head) {
     var style = document.createElement('style');
     style.type = 'text/css';
-    head.appendChild(style);
     style.appendChild(document.createTextNode(css));
+    head.appendChild(style);
   }
 }) ();
 // -----
 (function () {
   // 放送予定の表示範囲設定
   document.body.addEventListener('load', showProgramEvent, true);
-  // 放送予定の表示範囲の選択
-  var sort_selector = null;
-  document.body.addEventListener('load', function (e) {
-    if (!sort_selector) {
-      sort_selector = $('div.a-sort-selector') [0];
-      //console.info(sort_selector);
-      sort_selector.appendChild(document.createTextNode(' '));
-      //var time_selector = document.createElement('div');
-      //time_selector.className = 'a-time-selector column small-12 medium-8 text-right';
-      //time_selector.style = 'margin: 14px 0; padding-right: 0; float: right;';
-      var time_select = document.createElement('select');
-      time_select.id = 'a-time-selector';
-      time_select.addEventListener('change', showProgramEvent, true);
-      sort_selector.appendChild(time_select);
-      //
-      time_select.appendChild(createOptionItem('放送済みのみ', 'time_period_none', false));
-      time_select.appendChild(createOptionItem('12時間以内まで', 'time_period_half', true));
-      time_select.appendChild(createOptionItem('1日(24時間)まで', 'time_period_1day', false));
-      time_select.appendChild(createOptionItem('2日(48時間)まで', 'time_period_2day', false));
-      time_select.appendChild(createOptionItem('3日(72時間)まで', 'time_period_3day', false));
-      //time_select.appendChild(createOptionItem('1週間', 'time_period_1week', false));
+  //$('div.c-load-more-button') [0].firstChild.addEventListener('click', showProgramEvent, true);
+  /*
+  //var script = document.createElement('script');
+  //script.type = 'text/javascript';
+  //script.appendChild(document.createTextNode('showProgramEvent();'));
+  //document.body.appendChild(script);
+  //console.info(script);
+  */
+  function getHiddenMsecAndCreateTimeSelector() { // 放送予定の表示範囲セレクター
+    var time_select = $('#a-time-selector') [0];
+    //console.info(time_select);
+    if (!time_select) {
+      var selector_parent = $('select.custom-select') [0].parentNode;
+      //console.info(selector_parent);
+      if (selector_parent) {
+        selector_parent.appendChild(document.createTextNode(' '));
+        //
+        time_select = document.createElement('select');
+        time_select.id = 'a-time-selector';
+        time_select.className = 'custom-select';
+        time_select.addEventListener('change', showProgramEvent, true);
+        selector_parent.appendChild(time_select);
+        //
+        time_select.appendChild(createOptionItem('放送済みのみ', 0, false));
+        time_select.appendChild(createOptionItem('12時間以内まで', 12 * 60 * 60 * 1000, true));
+        time_select.appendChild(createOptionItem('1日(24時間)まで', 24 * 60 * 60 * 1000, false));
+        time_select.appendChild(createOptionItem('2日(48時間)まで', 48 * 60 * 60 * 1000, false));
+        time_select.appendChild(createOptionItem('3日(72時間)まで', 72 * 60 * 60 * 1000, false));
+        //time_select.appendChild(createOptionItem('1週間', 7 * 24 * 60 * 60 * 1000, false));
+        time_select.appendChild(createOptionItem('すべて', - 1, false));
+      }
     }
-  }, true);
-  function createOptionItem(name, value, selected) {
+    return time_select.value;
+  }
+  function createOptionItem(name, value, selected) { // セレクターの選択肢作成
     var opt = document.createElement('option');
     opt.textContent = name;
     opt.setAttribute('value', value);
     if (selected) opt.setAttribute('selected', 'selected');
     return opt;
-  } //
-  // 放送予定の表示範囲設定
-  function showProgramEvent() {
-    var hidden_msec = 12 * 60 * 60 * 1000;
-    var time_select = $('#a-time-selector');
-    //console.info(time_select);
-    if (time_select && time_select.length > 0) {
-      var time_select_value = time_select[0].value;
-      //console.info(time_select_value);
-      switch (time_select_value) {
-        case 'time_period_none':
-          hidden_msec = 0;
-          break;
-        case 'time_period_half':
-          hidden_msec = 12 * 60 * 60 * 1000;
-          break;
-        case 'time_period_1day':
-          hidden_msec = 24 * 60 * 60 * 1000;
-          break;
-        case 'time_period_2day':
-          hidden_msec = 48 * 60 * 60 * 1000;
-          break;
-        case 'time_period_3day':
-          hidden_msec = 72 * 60 * 60 * 1000;
-          break;
-        case 'time_period_1week':
-          hidden_msec = 7 * 24 * 60 * 60 * 1000;
-          break;
-      }
-    }
+  }
+  function showProgramEvent() { // 放送予定の表示設定
+    var hidden_msec = getHiddenMsecAndCreateTimeSelector(); // 12 * 60 * 60 * 1000;
+    //console.info(hidden_msec);
     var now_dt = new Date();
     // 非表示とする放送予定の時刻(通算ミリ秒, now_dt.getTime() + 12 * 60 * 60 * 1000 で現在時刻より12時間前)
-    var hidden_dt = new Date(now_dt.getTime() + hidden_msec);
+    var hidden_dt = hidden_msec >= 0 ? new Date(now_dt.getTime() + hidden_msec * 1)  : new Date('2099/12/31 23:59:59.999');
+    //console.info(hidden_dt);
     // 放送中と判断する時刻(通算ミリ秒, now_dt.getTime() - 30 * 60 * 1000 で現在時刻より30分前)
     var before_dt = new Date(now_dt.getTime() - 30 * 60 * 1000);
-    //console.info(hidden_dt);
     // ---------- ----------
-    $('div.a-program-container').each(function () {
-      var started_at = $(this).find('span.a-started-at') [0];
+    $('div.c-card>div.row').each(function () {
+      var started_at = $(this).find('span.mr-zp5') [0];
       //console.info(started_at);
       this.style.display = '';
       var started_at_obj = new Date('2017/' + started_at.textContent);
